7b3183c64b29fb2788162e48fb9d13c008647121
Tamerlan <tamerlan.teshev@icloud.com>
2022-09-18 15:40:35 +0300

    Add Program File

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -1,1 +1,8 @@
 ﻿
+Console.WriteLine ("Введите некоторые строки разной длины через пробел \n");
+string text = Console.ReadLine ();
+string [] array = text.Split(' ');
+for (int i = 0; i < array.Length; i++)
+    {   
+        if (array[i].Length <=3) Console.WriteLine(array[i]);
+    }
